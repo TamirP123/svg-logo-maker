d2ac70526e10023555e6b302ac7b2a317c8ce2cb
Tamir <tamirphillips@live.com>
2023-11-10 15:35:36 -0500

    Finished functionality for shape render() method.

--- shapes.js
+++ shapes.js
@@ -4,7 +4,6 @@ class Shape {
     this.textColor = textColor;
     this.shapeColor = shapeColor;
   }
-
 }
 
 class Square extends Shape {
@@ -13,7 +12,10 @@ class Square extends Shape {
   }
 
   render() {
-    
+    return `<svg width="400" height="110">
+    <rect width="300" height="100" fill="${this.shapeColor}" />
+    <text x="150" y="50" font-size="45" text-anchor="middle" fill="${this.textColor}">${this.text}</text>
+  </svg>`
   }
 }
 
@@ -23,7 +25,10 @@ class Triangle extends Shape {
   }
 
   render() {
-
+    return `<svg height="500" width="500">
+    <polygon points="250,60 100,400 400,400" class="triangle" fill="${this.shapeColor}" />
+    <text x="255" y="265" font-size="60" text-anchor="middle" fill="${this.textColor}">${this.text}</text>
+    </svg>`;
   }
 }
 
@@ -35,11 +40,11 @@ class Circle extends Shape {
   render() {
     return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
 
-    <circle cx="150" cy="100" r="80" fill="green" />
+    <circle cx="150" cy="100" r="80" fill="${this.shapeColor}" />
   
-    <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">SVG</text>
+    <text x="150" y="125" font-size="60" text-anchor="middle" fill="${this.textColor}">${this.text}</text>
   
-  </svg>`
+  </svg>`;
   }
 }
 
